21bbaa3c0dc0058ede5c0367e1439e28d3877dac
extra2005 <extra2005@mail.ru>
2018-02-09 12:55:33 +0300

    Update config.php
    Обновил

--- app/config.php
+++ app/config.php
@@ -3,6 +3,6 @@
 			
 $conf['db_host']="localhost";		//MySQL cервер
 $conf['db_port']="3306";			//MySQL порт:
-$conf['db_user']="verna";		    //Логин для базы данных:
-$conf['db_password']="verna";       //Пароль для базы данных:
-$conf['db_name']="verna";			//База данных MySQL:
+$conf['db_user']="";		    //Логин для базы данных:
+$conf['db_password']="";       //Пароль для базы данных:
+$conf['db_name']="";			//База данных MySQL:
